15a1f25e2eb623b1b279fb405bc543090f9a7107
ZARANT <zhaozhaoran@outlook.com>
2020-08-18 15:24:15 +0800

    the 1st&2nd exercise

---
+++ 动手实践01_钱币检测/__init__.py
@@ -0,0 +1,4 @@
+# author:nannan
+# contact: zhaozhaoran@bupt.edu.cn
+# datetime:2020/8/18 2:57 下午
+# software: PyCharm
---
+++ 动手实践02_词袋模型图片分类/__init__.py
@@ -0,0 +1,4 @@
+# author:nannan
+# contact: zhaozhaoran@bupt.edu.cn
+# datetime:2020/8/18 3:23 下午
+# software: PyCharm
